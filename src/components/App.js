2f15e711b97db3bc122c1150bfbbe4fe18b01846
Yogesh3712 <113026686+Yogesh3712@users.noreply.github.com>
2024-08-28 20:21:58 +0530

    chsnges

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,50 @@
 
-import React from 'react';
-import TodoList from './ToDoList';
+import React, { useState } from "react";
+import './../styles/App.css';
+import { v4 as uuidv4 } from 'uuid';
+
+const App = () => {
+
+  const[todos, setTodos] = useState([])
+  const[text, setText] = useState("")
+
+  function deleteBtnHandler(){
+
+  }
 
 
-
-
-function App() {
   return (
-    <div className="App">
-     <TodoList/>
+    <div>
+      <h3>To-Do List</h3>
+        <input value={text} onChange={(e) => {
+          setText(e.target.value)
+        }} />
+        <button onClick={(e) => {
+          if(!text) return
+
+          const newTodo = {
+            task : text,
+            id : uuidv4()
+          }
+
+          setTodos([...todos, newTodo])
+
+          setText("")
+        }}>Add Todo</button>
+
+        <ul>
+          {todos && todos.map((item) => {
+            return <li style={{listStyle : "none"}} key={item.id}>{item.task}  <button onClick={() => {
+              const narr = todos.filter((todo) => {
+                return todo.id != item.id
+              })
+
+              setTodos(narr)
+            }}>Delete</button></li>
+          })}
+        </ul>
     </div>
-  );
+  )
 }
 
-export default App;
-
+export default App
